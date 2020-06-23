b9ede591e5fea4de584423409248b4fcb78b9cbf
Greyson Rampazo <greysonf@gmail.com>
2020-06-22 23:48:24 -0300

    first commit

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const Post = use('App/Models/Post')
+
+class PostController {
+  async index({ request, auth }) {
+    const user = await auth.getUser()
+
+    if (await user.can('read_private_post')) {
+      const posts = await Post.all()
+
+      return posts
+    }
+
+    const posts = await Post.query().where({ type: 'public' }).fetch()
+
+    return posts
+  }
+
+  async store({ request, auth }) {
+    const data = request.only(['title', 'content', 'type'])
+
+    const post = await Post.create({ ...data, user_id: auth.user.id })
+
+    return post
+  }
+
+  async show({ params, auth, response }) {
+    const post = await Post.findOrFail(params.id)
+
+    if (post.type === 'public') {
+      return post
+    }
+
+    const user = await auth.getUser()
+
+    if (await user.can('read_private_post')) {
+      return post
+    }
+
+    return response.status(400).send({
+      error: {
+        message: 'You are not allowed to read this post.'
+      }
+    })
+
+  }
+
+  async update({ params, request, response }) {
+    const data = request.only(['title', 'content', 'type'])
+
+    const post = await Post.findOrFail(params.id)
+
+    post.merge(data)
+
+    await post.save()
+
+    return post
+  }
+
+  async destroy({ params }) {
+    const post = await Post.findOrFail(params.id)
+
+    await post.delete()
+  }
+}
+
+module.exports = PostController
---
+++ start/routes.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+const Route = use('Route')
+
+Route.post('/sessions', 'SessionController.store')
+
+Route.post('/users', 'UserController.store')
+Route.get('/users', 'UserController.index').middleware('auth')
+Route.put('/users/:id', 'UserController.update').middleware('auth')
+
+Route.resource('/posts', 'PostController')
+  .apiOnly()
+  .except(['index', 'show'])
+  .middleware(['auth', 'is:(administrator || moderator)'])
+
+Route.get('/posts', 'PostController.index')
+  .middleware(['auth', 'can:(read_post || read_private_post)'])
+
+Route.get('/posts/:id', 'PostController.show')
+  .middleware(['auth', 'can:(read_post || read_private_post)'])
+
+Route.resource('/permissions', 'PermissionController')
+  .apiOnly()
+  .middleware('auth')
+
+Route.resource('/roles', 'RoleController')
+  .apiOnly()
+  .middleware('auth')
